6c2f8f6d870402dc1c92aa759bccc403804cf044
Nick McKay <nickmckay@users.noreply.github.com>
2018-04-25 19:38:29 -0700

    initial commit - all tables are read in and stored in R

---
+++ scratch.R
@@ -0,0 +1,12 @@
+library(RMySQL)
+library(here)
+sisal = dbConnect(MySQL(),user = "root",dbname = 'sisal')#connect to database (only works if you have MySQL setup and the Sisal my sql database)
+
+allFields <- dbListTables(sisal) #get all the fields
+
+all <- vector(mode = "list",length = length(allFields))
+for(i in 1:length(all)){#loop through and get all the data
+  all[[allFields[i]]] <- dbReadTable(sisal, name = allFields[i])
+}
+
+save(list = "all",here("allTables.RData"))
